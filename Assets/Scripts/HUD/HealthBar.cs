c5abf865ad9950c83b2beb477da1c1b98ffa09d1
Serg T <nonehill@mail.ru>
2015-10-18 23:26:29 +0300

    healthBar created

---
+++ Assets/Scripts/HUD/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	public RectTransform canavasRect;
+	public RectTransform healtBarRect;
+	public Transform enemyFollow;
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 v2 = RectTransformUtility.WorldToScreenPoint (Camera.main, enemyFollow.position);
+		healtBarRect.anchoredPosition = v2 - canavasRect.sizeDelta / 2 + new Vector2(0, 50);
+	}
+}
