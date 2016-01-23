5f96dc90ae1ee989af6f709b6b8ce0f8a753efef
Serg T <nonehill@mail.ru>
2016-01-24 00:47:02 +0200

    menu windows added

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+
+public enum WindowKind
+{
+    Challenge = 0,
+    Cards = 1,
+    Setings = 2
+}
 
 public class Menu : MonoBehaviour {
 
 	public AudioSource menuMusic;
 	public GameObject cardsWindow;
 	public GameObject dailyChallengeWindow;
+    public GameObject settingWindow;
 
 	// Use this for initialization
 	void Start () 
 	{
+        System.Object ob = null;//new Object();
+        try
+        {
+            ob.ToString();
+        }
+        catch (Exception ex)
+        {
+            Debug.LogWarning("error " + ex);
+        }
+        finally
+        {
+                        Debug.LogWarning("success");
+        }
 	}
 
 	public void PlayPressed () 
@@ -27,12 +49,23 @@ public class Menu : MonoBehaviour {
 		dailyChallengeWindow.SetActive (true);
 	}
 
-	public void BackPressed ()
+	public void BackPressed (int windowKind)
 	{ 
-		if (dailyChallengeWindow.activeSelf)
-		{
-			dailyChallengeWindow.SetActive (false);
-		}
+        switch ((WindowKind)windowKind)
+        {
+            case WindowKind.Challenge:
+                dailyChallengeWindow.SetActive(false);
+                break;
+                
+            case WindowKind.Cards:
+                cardsWindow.SetActive(false);
+                break;
+                
+            case WindowKind.Setings:
+                settingWindow.SetActive(false);
+                break;
+        }
+        Debug.LogWarning("BackPressed + " + transform.parent);
 	}
 
 	public void FacebookPressed ()
